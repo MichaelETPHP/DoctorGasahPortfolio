fef957e8c171f8d85cd69ceb60c3b8ee38e0f914
MichaelETPHP <michaeltaye200@gmail.com>
2025-04-20 22:53:50 +0300

    Update - conf2

--- node_modules/workspace/node_modules/workspace/.svelte-kit/output/server/chunks/internal.js
+++ node_modules/workspace/node_modules/workspace/.svelte-kit/output/server/chunks/internal.js
@@ -1741,7 +1741,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "4nyz6h"
+  version_hash: "1x1by38"
 };
 async function get_hooks() {
   let handle;
