43a6d76025e65f9ec7ca76009cf301a46ce15307
MichaelETPHP <michaeltaye200@gmail.com>
2025-04-20 09:17:00 +0300

    Initial commit

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -8,18 +8,18 @@
         </p>
         <div class="flex space-x-4">
           <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" class="w-10 h-10 rounded-full bg-blue-800 hover:bg-blue-700 flex items-center justify-center transition-colors" aria-label="LinkedIn Profile">
-            <svg class="feather h-5 w-5">
-              <use href="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather-sprite.svg#linkedin"/>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
             </svg>
           </a>
           <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" class="w-10 h-10 rounded-full bg-blue-800 hover:bg-blue-700 flex items-center justify-center transition-colors" aria-label="Twitter Profile">
-            <svg class="feather h-5 w-5">
-              <use href="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather-sprite.svg#twitter"/>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
             </svg>
           </a>
           <a href="#contact" class="w-10 h-10 rounded-full bg-blue-800 hover:bg-blue-700 flex items-center justify-center transition-colors" aria-label="Contact Via Email">
-            <svg class="feather h-5 w-5">
-              <use href="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather-sprite.svg#mail"/>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
           </a>
         </div>
